e2d360a3579cfbe0b6545d5a0ba4e582f1201dc3
RolanAbbasguliev <abbarolan@gmail.com>
2023-03-03 21:43:27 +0300

    business and package section added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 import '../styles/main.scss';
 
+// let item_container = document.getElementsByClassName('business-dir__grid-item');
 
+// Array.from(item_container).forEach((element) => {
+//   let grid_item_text = element.childNodes[3].lastElementChild;
+
+//   element.addEventListener('mouseenter', (event) => {
+//     let interval = setInterval(() => {
+//       grid_item_text.style.display = 'block';
+//       // console.log(event);
+//       // console.log(event.target.childNodes[3].lastElementChild);
+//       // grid_text = event.target.childNodes[3].lastElementChild;
+//       // grid_container = event.target.childNodes[3];
+//       // console.log(grid_text.innerText);
+//       // console.log(grid_container);
+//       // grid_text.style.display = 'block';
+//       clearInterval(interval);
+//     }, 500);
+//   });
+//   element.addEventListener('mouseout', (event) => {
+//     grid_item_text.style.display = '';
+//     console.log('OUT');
+//   });
+// });
